9f90a31bdacfcbbd7cf69467d453e923f7678d7c
Ronald Hensbergen <ronaldhensbergen@gmail.com>
2016-11-03 22:55:56 +0100

    Second commit for Sierpinksi

--- sierpinski.R
+++ sierpinski.R
@@ -1,22 +1,16 @@
-sierpinski <- function() {
+sierpinski <- function(A=c(0,0),B=c(2,0),C=c(1,1),Z=c(0,1),div=0.5,iterations=10000) {
         set.seed(1234)
-        A<-data.frame(X=0,Y=0)
-        B<-data.frame(X=2,Y=0)
-        C<-data.frame(X=1,Y=1)
-        
+
         set<-rbind(A,B,C)
-        Z<-data.frame(X=0,Y=1)
-        div<-0.5
-        iterations = 10000
-        
+
         result<-data.frame(X=rep(0,iterations),Y=rep(0,iterations),
                            dice=sample(1:3,iterations,replace=TRUE))
-        result[1,1]<-Z$X
-        result[1,2]<-Z$Y
+        result[1,1]<-Z[1]
+        result[1,2]<-Z[2]
         for (i in 2:iterations) {
                 result[i,1]<-(result[i-1,1]+set[result[i,3],1])*div
                 result[i,2]<-(result[i-1,2]+set[result[i,3],2])*div
         }
         
-        plot(result$X,result$Y)
+        plot(result$X,result$Y,pch=20)
 }
